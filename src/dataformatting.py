02eb22c055d64342d666c3bcc0d81cf3535b5f5f
samhiner <25464398+samhiner@users.noreply.github.com>
2018-08-23 21:43:21 -0400

    Make script store image pixels in memory.
    This doesn't work. Just adding it for proof for personal project

--- src/dataformatting.py
+++ src/dataformatting.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
 from PIL import Image
+import sys
 
 #get a list of all of the objid's in the database
 zoo_data = pd.read_csv('../data/zoodata.csv', sep=',', header=0)
 filenames = list(zoo_data.loc[:,'OBJID'])
 
-arr_file = open('../data/pixelsarray.txt' , 'a')
+image_arr = []
 
 #convert all of the images to greyscale arrays of pixels and write those arrays to a file
 for file in range(0,len(filenames)):
@@ -16,14 +17,12 @@ for file in range(0,len(filenames)):
 
 	objArr = objArr[:,0] / 255
 
-	arr_file.write(str(list(objArr)) + ''',
-	''')
+	image_arr.append(objArr)
 
 	if file % 10000 == 0:
 		print('On file: ' + str(file))
 
 #add an ending bracket to the file so it can be evaluated as an array of arrays of pixels (aka an array of "images")
-arr_file.write(']')
-arr_file.close()
+sys.getsizeof(array)
 
 print('Done')
